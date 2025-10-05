e18f954c80ae3c23fa048193e350e7b9b1883b4d
RockLee0 <mshakil3847@gmail.com>
2025-10-04 22:28:09 -0700

    User interface model controller route globalerrorHandler NotFoundHandler Middlewares created

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
 import express, { Request, Response } from "express";
-
+import cors from "cors";
+import {UserRoutes } from "./app/modules/user/user.route";
+import { router } from "./app/routes";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import { notFoundErrorHandler } from "./app/middlewares/notFoundErrorHandler";
 
 const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+app.use("/api/v1", router)
 app.get("/", (req: Request, res: Response)=>{
     res.status(200).json({
         message: "welcome to tour management backend"
     })
 })
+
+app.use(globalErrorHandler)
+app.use(notFoundErrorHandler)
+
 
 export default app;
